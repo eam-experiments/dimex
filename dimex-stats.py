93c8e2e3927a515aea8a8a05755a13f93dd83e78
Rafael Morales-Gamboa <rmorales@suv.udg.mx>
2021-07-18 14:38:53 -0500

    Better sampling of data to reduce unbalancing of frequences.

--- dimex-stats.py
+++ dimex-stats.py
@@ -12,24 +12,24 @@ for label in frequencies:
 # Load DIMEX-100 labels
 labels = np.load('Features/feat_Y.npy')
 features = np.load('Features/feat_X.npy', allow_pickle=True)
+data = [(features[i], labels[i]) for i in range(0, len(features))]
+random.shuffle(data)
 
 for label in labels:
     frequencies[label] += 1
 
 # Reduce the number of instances of over-represented classes
 pairs = []
-for i in range(len(features)):
-    label = labels[i]
-    if frequencies[label] > cut_point:
-        frequencies[label] -= 1
+for f, l in data:
+    if frequencies[l] > cut_point:
+        frequencies[l] -= 1
     else:
-        pairs.append((label, features[i]))
+        pairs.append((f, l))
 
 random.shuffle(pairs)
 
-for label, desc in pairs:
-    labels[i] = label
-    features[i] = desc
+features = [p[0] for p in pairs]
+labels = [p[1] for p in pairs]
 
 np.save('Features/rand_Y.npy', labels)
 np.save('Features/rand_X.npy', features)
