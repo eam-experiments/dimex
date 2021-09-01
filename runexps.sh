e58c4ccbe1116a8c1e2a6bd6cfa13d7e0062baf3
Rafael Morales Gamboa <r.moralesgamboa@gmail.com>
2021-09-01 08:47:12 -0500

    Updated runexps.sh, adding condition of success to continue on next stages.

--- runexps.sh
+++ runexps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-python main_test_associative.py -n
-python main_test_associative.py -f
-python main_test_associative.py -a
-python main_test_associative.py -e 1
-python main_test_associative.py -e 3
+python main_test_associative.py -n && \
+python main_test_associative.py -f && \
+python main_test_associative.py -a && \
+python main_test_associative.py -e 1 && \
+python main_test_associative.py -e 3 && \
 python main_test_associative.py -e 4
