5f00dc8c5eeaac7f3d5e9aeece3c6407a12298fd
Rafael Morales Gamboa <r.moralesgamboa@gmail.com>
2021-09-03 23:58:30 -0500

    learned_stats.py to be fixed.

--- learned_stats.py
+++ learned_stats.py
@@ -1,14 +1,18 @@
 import argparse
 import csv
+import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
 import constants
+import dimex
 
 AGREED = 'agreed'
 ORIGINAL = 'original'
 MEMORY = 'memory'
 NETWORK = 'network'
+SEED_LABELS = 'runs/seed_balanced_Y.npy'
+EXPERIMENT = 4
 
 suffixes = {
     AGREED : constants.agreed_suffix,
@@ -16,16 +20,40 @@ suffixes = {
     MEMORY: constants.amsystem_suffix,
     NETWORK: constants.nnetwork_suffix}
 
+def seed_frequencies():
+    count = np.zeros(constants.n_labels, dtype=int)
+    labels = np.load(SEED_LABELS)
+    for label in labels:
+        count[label] += 1
+    return count
+
+def plot_graph(suffix, stage, bot, top, top_err):
+    plt.clf()
+    labels = dimex.labels_to_phns
+    width = 3
+    plt.bar(labels, bot, width, label='Seed')
+    plt.bar(labels, top, width, yerr=top_err, bottom=bot, label='Learned')
+    plt.ylabel('Data')
+    plt.xlabel('Phonemes')
+    plt.legend()
+    suffix = constants.learning_data_learnt + suffix
+    filename = constants.picture_filename(suffix, EXPERIMENT, stage=stage)
+    plt.savefig(filename, dpi=600)
+
+
 def get_stats(suffix, stage):
-    stats = np.zeros((constants.n_labels, constants.n_folds))
+    seed = seed_frequencies()
+    stats = np.zeros((constants.n_labels, constants.n_folds), dtype=int)
     for fold in range(constants.n_folds):
         filename = constants.learned_labels_filename(suffix, fold, stage)
         labels = np.load(filename)
+        count = np.zeros(constants.n_labels, dtype=int)
         for label in labels:
-            stats[labels,fold] += 1
-    f = csv.writer(sys.stdout)
-    for label in range(constants.n_labels):
-        f.writerow(stats[label,:])
+            count[label] += 1
+        stats[:,fold] = count
+    means = np.mean(stats, axis = 1)
+    stdvs = np.std(stats, axis = 1)
+    plot_graph(suffix, stage, seed, means, stdvs)
 
 if __name__== "__main__" :
     parser = argparse.ArgumentParser(description='Analysis of learned data in experiment 4.')
