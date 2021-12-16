bb49c77b96a3a0b6b8b3bcf9a03bac8ba9870363
Rafael Morales Gamboa <r.moralesgamboa@gmail.com>
2021-12-16 00:05:22 -0600

    Added parameter to reduce memory fills in experiments.

--- runexps.sh
+++ runexps.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 for i in 0 1 2 3 4 5 6 7 8; do
     echo "=================== Starting stage ${i}..."
-    python eam.py -n $i && \
-    python eam.py -f $i && \
-    python eam.py -a $i && \
-    python eam.py -e $i && \
-    python eam.py -i $i && 
+    python eam.py -n $i -m 64 && \
+    python eam.py -f $i -m 64 && \
+    python eam.py -a $i -m 64 && \
+    python eam.py -e $i -m 64 && \
+    python eam.py -i $i -m 64 && 
     echo "=================== Stage $i finished."
 done
