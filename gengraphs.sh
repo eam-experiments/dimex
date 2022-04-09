16e039d47d08ed31940059f6448d4ff5236f77cb
Rafael Morales Gamboa <r.moralesgamboa@gmail.com>
2022-04-08 21:39:35 -0500

    Gengraph.sh can now generate graphs for the extended and unextended version of the last learning stage.

--- gengraphs.sh
+++ gengraphs.sh
@@ -1,30 +1,42 @@
 #! /bin/bash
 
+learned=4
+tolerance=0
+sigma=10
+
 prse=graph_prse_MEAN-english-stg_00
 overall=overallgraph_prse_MEAN-english-stg_00
 gsize=graph_size_MEAN-english-stg_00
 behav=graph_behaviours_MEAN-english-stg_00
 recall=recall-graph_prse_MEAN-english-stg_00
 totalr=total_recall-graph_prse_MEAN-english-stg_00
-suffix_gral=lrn_001-tol_003.png
-suffix_nine=lrn_001-ext-tol_003.png
-for i in 0 1 2 3 4 5 6 7 8 9; do
+suffix_gral=lrn_00${learned}-tol_00${tolerance}.png
+suffix_nine=lrn_00${learned}-ext-tol_00${tolerance}.png
+for i in 0 1 2 3 4 5 6 7 8 9 10; do
    if [ $i == 9 ]; then
         suffix=$suffix_nine
+        img_suffix="-x"
     else
         suffix=$suffix_gral
+        img_suffix=""
     fi
-    cd runs/stage_$i
+    if [ $i == 10 ]; then
+        j=9
+    else
+        j=$i
+    fi
+
+    cd runs-32-d${learned}-t${tolerance}-s${sigma}/stage_$j
 	for f in *.svg; do
 		g=`basename $f .svg`
 		rsvg-convert -f png -o ${g}.png $f
 	done
 
-	convert $prse${i}-$suffix $overall${i}-$suffix +append row1.png
-	convert $gsize${i}-$suffix $behav${i}-$suffix +append row2.png
-	convert $recall${i}-$suffix $totalr${i}-$suffix +append row3.png
-	convert row1.png row2.png row3.png -append stage_${i}.png
-        cp stage_${i}.png ..
+	convert $prse${j}-$suffix $overall${j}-$suffix +append row1.png
+	convert $gsize${j}-$suffix $behav${j}-$suffix +append row2.png
+	convert $recall${j}-$suffix $totalr${j}-$suffix +append row3.png
+	convert row1.png row2.png row3.png -append stage_${j}${img_suffix}.png
+        cp stage_${j}${img_suffix}.png ..
 	cd ../..
 done
 
