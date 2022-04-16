0dcf9f71c8b6f25ce81d41c6868c97778f2b6495
Rafael Morales Gamboa <r.moralesgamboa@gmail.com>
2022-04-15 22:15:16 -0500

    Gengraphs.sh now for all stages extended, or none.

--- gengraphs.sh
+++ gengraphs.sh
@@ -2,7 +2,8 @@
 
 learned=4
 tolerance=0
-sigma=10
+sigma=50
+iota=1
 
 prse=graph_prse_MEAN-english-stg_00
 overall=overallgraph_prse_MEAN-english-stg_00
@@ -10,23 +11,11 @@ gsize=graph_size_MEAN-english-stg_00
 behav=graph_behaviours_MEAN-english-stg_00
 recall=recall-graph_prse_MEAN-english-stg_00
 totalr=total_recall-graph_prse_MEAN-english-stg_00
-suffix_gral=lrn_00${learned}-tol_00${tolerance}.png
-suffix_nine=lrn_00${learned}-ext-tol_00${tolerance}.png
+suffix=lrn_00${learned}-ext-tol_00${tolerance}.png
+img_suffix="-x"
 for i in 0 1 2 3 4 5 6 7 8 9 10; do
-   if [ $i == 9 ]; then
-        suffix=$suffix_nine
-        img_suffix="-x"
-    else
-        suffix=$suffix_gral
-        img_suffix=""
-    fi
-    if [ $i == 10 ]; then
-        j=9
-    else
-        j=$i
-    fi
-
-    cd runs-32-d${learned}-t${tolerance}-s${sigma}/stage_$j
+    j=$i
+    cd runs-32-d${learned}-t${tolerance}-s${sigma}-i${iota}-x/stage_$j
 	for f in *.svg; do
 		g=`basename $f .svg`
 		rsvg-convert -f png -o ${g}.png $f
