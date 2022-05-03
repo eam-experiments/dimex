9fb86ee905812cb870838641bed44d08fbbcee9a
Rafael Morales Gamboa <r.moralesgamboa@gmail.com>
2022-05-03 00:49:57 -0500

    Recognition is now of the whole set per fold.

--- dimex.py
+++ dimex.py
@@ -204,11 +204,12 @@ class Sampler:
     def get_samples(self, fold):
         # We use this to explore results without working
         # with the whole data.
-        n_folds = max(constants.n_folds, 10)
-        per_fold = (int)(len(self.testing_data)/n_folds)
-        start = fold*per_fold
-        end = start + per_fold
-        return self.testing_data[start:end]
+        # n_folds = max(constants.n_folds, 10)
+        # per_fold = (int)(len(self.testing_data)/n_folds)
+        # start = fold*per_fold
+        # end = start + per_fold
+        # return self.testing_data[start:end]
+        return self.testing_data
 
     def _get_ids(self, filename):
         ids = []
