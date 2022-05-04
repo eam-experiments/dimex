8ece37904aba7ab7b288506c66a79151f0af0aee
Rafael Morales Gamboa <r.moralesgamboa@gmail.com>
2022-05-04 13:35:59 -0500

    Updated script for running the experiments.

--- runexps.sh
+++ runexps.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
+########### Script de Ejecución (en Slurm) #########
+#SBATCH --partition=gpu
+#SBATCH --nodelist=nvd02
+#SBATCH --cpus-per-task=22
+#SBATCH --error=job.%J.err
+#SBATCH --output=job.%J.out
 
-if [ $# != 2 ]; then
-    echo "Usage: $0 learned tolerance"
-    exit 1
-fi
-learned=$1
-tolerance=$2
-for i in 0 1 2 3 4 5 6 7 8 9; do
-    if [ $i == 9 ]; then
-        extended="-x"
+# source ~/.bash_profile
+# cd dimex
+# conda activate eam
+learned=4
+tolerance=0
+sigma=0.50
+iota=0.0
+kappa=0.0
+extended="-x"
+runpath="runs-d$learned-t$tolerance-i$iota-k$kappa-s$sigma"
+ok=1
+echo "Storing results in $runpath"
+for i in 0 1 2 3 4 5 ; do
+    j=$i
+    if [  $ok != 1 ]; then
+        exit 1
     else
-        extended=""
+	ok=0
     fi
     echo "=================== Starting stage ${i}..."
-    python eam.py -n $i --learned=$learned --tolerance=$tolerance $extended && \
-    python eam.py -f $i --learned=$learned --tolerance=$tolerance $extended && \
-    python eam.py -a $i --learned=$learned --tolerance=$tolerance $extended && \
-    python eam.py -e $i --learned=$learned --tolerance=$tolerance $extended && \
-    python eam.py -i $i --learned=$learned --tolerance=$tolerance $extended && 
+    python eam.py -n $j --learned=$learned --tolerance=$tolerance $extended --sigma=$sigma --iota=$iota --kappa=$kappa --runpath=$runpath && \
+    python eam.py -f $j --learned=$learned --tolerance=$tolerance $extended --sigma=$sigma --iota=$iota --kappa=$kappa --runpath=$runpath && \
+    python eam.py -a $j --learned=$learned --tolerance=$tolerance $extended --sigma=$sigma --iota=$iota --kappa=$kappa --runpath=$runpath && \
+    python eam.py -e $j --learned=$learned --tolerance=$tolerance $extended --sigma=$sigma --iota=$iota --kappa=$kappa --runpath=$runpath && \
+    python eam.py -i $j --learned=$learned --tolerance=$tolerance $extended --sigma=$sigma --iota=$iota --kappa=$kappa --runpath=$runpath && \
+    python eam.py -r $j --learned=$learned --tolerance=$tolerance $extended --sigma=$sigma --iota=$iota --kappa=$kappa --runpath=$runpath &&
+    ok=1
     echo "=================== Stage $i finished."
 done
+
