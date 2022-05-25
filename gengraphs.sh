ee93d39d4860644a0c1ace553796cd94222904c1
Rafael Morales Gamboa <r.moralesgamboa@gmail.com>
2022-05-25 15:55:30 -0500

    Added command line arguments to gengraph.sh

--- gengraphs.sh
+++ gengraphs.sh
@@ -1,10 +1,44 @@
 #! /bin/bash
 
+stages=6
 learned=4
 tolerance=0
-sigma=0.10
-iota=0.3
-kappa=1.5
+sigma=0.50
+iota=0.0
+kappa=0.0
+
+for i in "$@"; do
+  case $i in
+    -g=*|--stages=*)
+      stages="${i#*=}"
+      shift
+      ;;
+    -l=*|--learned=*)
+      learned="${i#*=}"
+      shift
+      ;;
+    -t=*|--tolerance=*)
+      tolerance="${i#*=}"
+      shift 
+      ;;
+    -s=*|--sigma=*)
+      sigma="${i#*=}"
+      shift 
+      ;;
+    -i=*|--iota=*)
+      iota="${i#*=}"
+      shift 
+      ;;
+    -k=*|--kappa=*)
+      kappa="${i#*=}"
+      shift 
+      ;;
+    *)
+      echo "Usage $0: [-l | --learned]=learned..."
+      exit 1
+      ;;
+  esac
+done
 
 prse=graph_prse_MEAN-english-stg_00
 overall=overallgraph_prse_MEAN-english-stg_00
@@ -14,9 +48,14 @@ recall=recall-graph_prse_MEAN-english-stg_00
 totalr=total_recall-graph_prse_MEAN-english-stg_00
 suffix=lrn_00${learned}-ext-tol_00${tolerance}.png
 img_suffix="-x"
-for i in 0 1 2 3 4 5 6 7 8 9 ; do
-    j=$i
-    cd runs-d${learned}-t${tolerance}-i${iota}-k${kappa}-s${sigma}/stage_$j
+for ((i=0; i<stages;i++)); do
+  j=$i
+  runs="runs-d${learned}-t${tolerance}-i${iota}-k${kappa}-s${sigma}/stage_$j"
+  if [ ! -d $runs ]; then
+    echo "Directory $runs does not exist."
+    exit 2
+  fi
+  cd $runs
 	for f in *.svg; do
 		g=`basename $f .svg`
 		rsvg-convert -f png -o ${g}.png $f
