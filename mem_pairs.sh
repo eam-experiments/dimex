79b29736f003a2a2b807407e91b70307b8ea9242
Rafael Morales Gamboa <r.moralesgamboa@gmail.com>
2022-10-04 15:40:36 -0500

    Updated to individual parameters.

--- mem_pairs.sh
+++ mem_pairs.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/bash
 
 prefix="mem_confrix-lbl_"
-suffix="-stg_000-lrn_004-ext-tol_000.png"
-suffixfive="-stg_000-lrn_004-ext-tol_002.png"
+suffix="-stg_000-lrn_004-ext.png"
 inone="runs-d4-t0-i0.0-k0.0-s0.50"
 intwo="runs-d4-t0-i0.1-k0.0-s0.50"
 inthree="runs-d4-t0-i0.0-k1.5-s0.50"
@@ -12,7 +11,6 @@ outdir="runs"
 
 for i in $(seq -f "%03g" 0 21); do
   filename="$prefix$i$suffix"
-  filenamefive="$prefix$i$suffixfive"
   echo $filename
-  convert "$inone/$filename" "$intwo/$filename" "$inthree/$filename" "$infour/$filename" "$infive/$filenamefive" +append $outdir/$filename
+  convert "$inone/$filename" "$intwo/$filename" "$inthree/$filename" "$infour/$filename" "$infive/$filename" +append $outdir/$filename
 done
